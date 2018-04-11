39972d8e80afdae8623f51f3a63ebf757755150e
DethierY <https://github.com/DethierY/Algo.git>
2018-04-11 23:43:56 +0200

    création elements header

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
     imports: [
         CommonModule,
-        NgModule
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule,
+        MatToolbarModule
     ],
     exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule,
+        MatToolbarModule
     ]
 })
 export class MaterialModule { }
