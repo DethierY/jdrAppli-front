30672cecec95131fd42841504d166040eb14f343
DethierY <https://github.com/DethierY/Algo.git>
2018-04-18 22:31:12 +0200

    toolbar responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,6 +18,7 @@ import { SortOptionsComponent } from './sort-options/sort-options.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     MaterialModule,
     NoopAnimationsModule,
   ],
