b1aa28894aa38245d6a433e049c6da622fa89715
DethierY <https://github.com/DethierY/Algo.git>
2018-05-16 18:49:41 +0200

    calcule taille enfin fonctionel

--- src/app/creation-perso/creation-perso.component.ts
+++ src/app/creation-perso/creation-perso.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { GameCharacterService } from '../game-character.service';
 import { CharacterClassService } from '../character-class.service';
 import { RaceService } from '../race.service';
 import { UserPageComponent } from '../user-page/user-page.component';
+import { CharacterClass } from '../models';
 import { CharacterClass,
          GameCharacter,
          Race } from '../models';
@@ -12,23 +13,21 @@ import { NgForm,
          FormControl,
          Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-creation-perso',
   templateUrl: './creation-perso.component.html',
   styleUrls: ['./creation-perso.component.css']
 })
-export class CreationPersoComponent implements OnInit, AfterContentChecked {
+export class CreationPersoComponent implements OnInit {
 
   gameCharacter: GameCharacter = new GameCharacter();
   classControl = new FormControl('', [Validators.required]);
   characterClass: CharacterClass;
   characterClasses: CharacterClass[];
   race: Race;
-  races: Race[];
   sexes = [
-    'femme',
-    'homme'
+    {value: 'femme', viewValue: 'femme'},
+    {value: 'homme', viewValue: 'homme'}
   ];
   alliegeances = [
     'bien',
@@ -47,34 +46,42 @@ export class CreationPersoComponent implements OnInit, AfterContentChecked {
     this.characterClassService.getClassNameList().subscribe(
       classList => this.characterClasses = classList
     );
-    this.raceService.getRaceList().subscribe(
-      raceList => this.races = raceList
-    );
-  }
-
-  ngAfterContentChecked() {
-    if (typeof this.gameCharacter.sex !== 'undefined' && typeof this.gameCharacter.characterClass !== 'undefined') {
-      this.gameCharacter.height = this.setHeight();
-    }
+    console.log('ngOnInit()');
   }
 
   onSubmit() {
     this.gameCharacter.user = this.userPageComponent.getUser();
     this.gameCharacter.level = 1;
-    console.log (this.gameCharacter);
     this.gameCharacterService.createGameCharacter(this.gameCharacter).subscribe();
   }
 
-  setHeight(): any {
+  sexChanged(sexValue: string): void {
+    if (typeof this.gameCharacter.sex === 'undefined') {
+    } else {
+      if (typeof this.gameCharacter.characterClass === 'undefined') {
+      }
+    }
+    if (typeof this.gameCharacter.characterClass !== 'undefined') {
+      this.setHeight();
+    }
+  }
+
+  classChanged(classValue: CharacterClass): void {
+    if (typeof this.gameCharacter.sex !== 'undefined' && typeof this.gameCharacter.characterClass !== 'undefined') {
+      this.setHeight();
+    }
+  }
+
+  setHeight() {
     let modifier = 0;
-    let baseHeight = this.gameCharacter.characterClass.race.baseHeight;
-    for (let i = 0; i < this.gameCharacter.characterClass.race.heightModifier.numberOfDices; i++) {
-      modifier = modifier + ((Math.random() * this.gameCharacter.characterClass.race.heightModifier.numberOfSides) + 1);
-    }
-    if (this.gameCharacter.sex = 'femme') {
-      baseHeight = baseHeight - this.gameCharacter.characterClass.race.heightSexModifier;
-    }
-    return (baseHeight + modifier / 100).toFixed(2);
+      let baseHeight = this.gameCharacter.characterClass.race.baseHeight;
+      for (let i = 0; i < this.gameCharacter.characterClass.race.heightModifier.numberOfDices; i++) {
+        modifier = modifier + ((Math.random() * this.gameCharacter.characterClass.race.heightModifier.numberOfSides) + 1);
+      }
+      if (this.gameCharacter.sex === 'femme') {
+        baseHeight = baseHeight - this.gameCharacter.characterClass.race.heightSexModifier;
+      }
+      this.gameCharacter.height = +(baseHeight + modifier / 100).toFixed(2);
   }
 
 }
